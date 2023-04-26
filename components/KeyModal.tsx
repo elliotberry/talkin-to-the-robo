94a70e420b5b459445a165ee26f97b59467e263b
Azad-sl <85327306+Azad-sl@users.noreply.github.com>
2023-04-26 14:49:18 +0800

    Update KeyModal.tsx

--- components/KeyModal.tsx
+++ components/KeyModal.tsx
@@ -165,13 +165,16 @@ export default function KeyModal({ close }: { close: () => void }) {
               name="填入你的 OpenAI API Key"
               initialKey={apiKeyOpenAI}
               setKeyFun={setApiKeyOpenAI}
-              descriptionAboveInput="您需要一个OpenAI API密钥。您的API密钥存储在本地的浏览器上，永远不会被发送到其他地方。如果您没有API KEY，可在这里自助获取："
+              descriptionAboveInput={<p>您需要一个OpenAI API密钥。您的API密钥存储在本地的浏览器上，永远不会被发送到其他地方。如果您没有API KEY，可在这里自助获取："
               <a
                     target="_blank"
                     href="https://faka.aihub.ren"
                   >
                     Digital Shop
                   </a>.
+                  </p>
+               }
+                                     
               descriptionBelowInput={
                 <p>
                   → 如果您有账号请登官网获取API：{" "}
